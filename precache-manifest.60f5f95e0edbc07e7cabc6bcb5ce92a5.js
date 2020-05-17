98f0fddb18b9280859318aad237d2cc12f49a254
vahidsavabieh <v_savabieh@yahoo.com>
2020-05-17 15:56:32 +0430

    Updates

---
+++ precache-manifest.60f5f95e0edbc07e7cabc6bcb5ce92a5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaab76577a7a48a73ea427c0dfab412d",
+    "url": "/vahidsavabieh/chitchat.git/index.html"
+  },
+  {
+    "revision": "d84ab8f4626651ba1593",
+    "url": "/vahidsavabieh/chitchat.git/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "882c20cad62ed7674f5e",
+    "url": "/vahidsavabieh/chitchat.git/static/css/main.c0b54c9d.chunk.css"
+  },
+  {
+    "revision": "d84ab8f4626651ba1593",
+    "url": "/vahidsavabieh/chitchat.git/static/js/2.d9570a6a.chunk.js"
+  },
+  {
+    "revision": "882c20cad62ed7674f5e",
+    "url": "/vahidsavabieh/chitchat.git/static/js/main.c642b60a.chunk.js"
+  },
+  {
+    "revision": "17cd780a2ec4654efb61",
+    "url": "/vahidsavabieh/chitchat.git/static/js/runtime~main.f740f3cd.js"
+  },
+  {
+    "revision": "52ce4de2efeeb8b18dcbd379711224f3",
+    "url": "/vahidsavabieh/chitchat.git/static/media/Yekan.52ce4de2.ttf"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/vahidsavabieh/chitchat.git/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/vahidsavabieh/chitchat.git/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/vahidsavabieh/chitchat.git/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/vahidsavabieh/chitchat.git/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/vahidsavabieh/chitchat.git/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
